2fcbeaacaa7e59912acbf3949d28db4bd0b08727
C4T4PHR4CT <56405586+C4T4PHR4CT@users.noreply.github.com>
2021-03-04 17:51:45 +0100

    gitignore fix pt2

---
+++ source/EventManagerBackend (Rider)/EventManagerBackend/Controllers/AuthorController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using EventManagerBackend.Data;
+using EventManagerBackend.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventManagerBackend.Controllers
+{
+    [ApiController]
+    [Route("author")]
+    public class AuthorController : ControllerBase
+    {
+        private readonly IPersistence _persistence;
+        
+        public AuthorController(IPersistence persistence)
+        {
+            _persistence = persistence;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult PostAuthor([FromBody] Author author)
+        {
+            author.Books = new List<Book>();
+            try
+            {
+                //_persistence.PostAuthor(author);
+                return StatusCode(200);
+            }
+            catch
+            {
+                return StatusCode(400);
+            }
+        }
+        
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public List<Author> GetAuthors()
+        {
+            return null; //_persistence.GetAuthors();
+        }
+    }
+}
---
+++ source/EventManagerBackend (Rider)/EventManagerBackend/Data/IPersistence.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using EventManagerBackend.Model;
+
+namespace EventManagerBackend.Data
+{
+    public interface IPersistence
+    {
+        public void PostAuthor(Author author);
+        public List<Author> GetAuthors();
+        public void PostBook(int authorId, Book book);
+        public List<Book> GetBooks();
+        public void DeleteBook(int isbn);
+    }
+}
