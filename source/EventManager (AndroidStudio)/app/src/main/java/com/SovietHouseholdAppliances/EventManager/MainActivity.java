dab24bb59f6f3c428d993061209fe2984977d8f1
C4T4PHR4CT <56405586+C4T4PHR4CT@users.noreply.github.com>
2021-03-10 20:08:59 +0100

    android login screen

--- source/EventManager (AndroidStudio)/app/src/main/java/com/SovietHouseholdAppliances/EventManager/MainActivity.java
+++ source/EventManager (AndroidStudio)/app/src/main/java/com/SovietHouseholdAppliances/EventManager/MainActivity.java
@@ -2,13 +2,48 @@ package com.SovietHouseholdAppliances.EventManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText username;
+    EditText password;
+    CheckBox remember;
+    CheckBox cache;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+        
+        username = findViewById(R.id.login_username);
+        password = findViewById(R.id.login_password);
+        remember = findViewById(R.id.login_remember);
+        cache = findViewById(R.id.login_cache);
+        login = findViewById(R.id.login_button);
+
+        cache.setPaintFlags(cache.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+
+        remember.setOnClickListener(e -> {
+            if (remember.isChecked())
+            {
+                cache.setClickable(true);
+                cache.setPaintFlags(cache.getPaintFlags() & (~Paint.STRIKE_THRU_TEXT_FLAG));
+            }
+            else
+            {
+                cache.setClickable(false);
+                cache.setChecked(false);
+                cache.setPaintFlags(cache.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+            }
+        });
     }
 }
